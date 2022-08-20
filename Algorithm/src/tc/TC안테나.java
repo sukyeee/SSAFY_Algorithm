fbcc1230326dc32db988442ad47d5a257d154fd2
suky <1724271@donga.ac.kr>
2022-08-21 01:17:09 +0900

    study: 2022/08/21

--- Algorithm/src/tc/TC안테나.java
+++ Algorithm/src/tc/TC안테나.java
@@ -2,6 +2,7 @@ package tc;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class TC안테나 {
@@ -20,23 +21,18 @@ public class TC안테나 {
 			house[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int sum = 0;
-		int idx = -1;
-		int Min = Integer.MAX_VALUE;
-		// 집의 위치 중 하나를 안테나 위치로 지정
-		for(int i=0;i<N;i++) {
-			sum = 0;
-			for(int j=0;j<N;j++) {
-				sum += Math.abs(house[i] - house[j]);
-			}
-			if( sum < Min ) {
-				Min = sum;
-				idx = house[i];
-			}
+		int ans = 0;
+		Arrays.sort(house); // 오름차순 정렬 
+		
+		if(N%2 == 0) { // 짝수일 때
+			ans = house[N/2 - 1];
+			
+		} else { // 홀수 일 때 
+			ans = house[N/2];
 		}
 		
 	
-		System.out.println(idx);
+		System.out.println(ans);
 		
 	}
 
