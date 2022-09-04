0e86f3de4c9b21d2945d609b202b47988e4133a9
suky <1724271@donga.ac.kr>
2022-09-05 02:32:34 +0900

    2022/09/05

--- Algorithm/src/bj/BJ14888연산자끼워넣기.java
+++ Algorithm/src/bj/BJ14888연산자끼워넣기.java
@@ -12,47 +12,54 @@ public class BJ14888연산자끼워넣기 {
 	static int min = Integer.MAX_VALUE;
 	static int max = Integer.MIN_VALUE;
 	static int sum;
+	static int start;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		A = new int[N-1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		start = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N-1; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		plus_cnt = Integer.parseInt(st.nextToken());
 		minus_cnt = Integer.parseInt(st.nextToken());
 		mul_cnt = Integer.parseInt(st.nextToken());
 		div_cnt = Integer.parseInt(st.nextToken());
 		
-		dfs(0, 0, 0, 0, 0);
+		
+		dfs(0, 0, 0, 0, start, 0); // 덧셈 뺄셈 곱셈 나눗셈  합 깊이 
 		
 		
 		//결과가 최대인 것과 최소인 것
+		System.out.println(max);
+		System.out.println(min);
+		
 	}
 	
-	static void dfs(int plus, int minus, int mul, int div, int sum) {
+	static void dfs(int plus, int minus, int mul, int div, int sum, int depth) {
 		if( plus == plus_cnt && minus == minus_cnt && mul == mul_cnt && div == div_cnt) {
 			min = Math.min(min, sum);
 			max = Math.max(max, sum);
 			return;
 		}
 		
-		if(plus == plus_cnt || minus == minus_cnt || mul == mul_cnt || div == div_cnt ) return;
 		
-		for (int i = 0; i < plus_cnt; i++) {
-			dfs(plus + 1, minus, mul, div, sum + A[i]);
+		for (int i = plus; i < plus_cnt; i++) {
+			dfs(plus + 1, minus, mul, div, sum + A[depth], depth+1);
 		}
-		for (int i = 0; i < minus_cnt; i++) {
-			dfs(plus, minus + 1, mul, div, sum - A[i]);
+		for (int i = minus; i < minus_cnt; i++) {
+			dfs(plus, minus + 1, mul, div, sum - A[depth], depth+1);
 		}
-		for (int i = 0; i < mul_cnt; i++) {
-			dfs(plus, minus, mul + 1, div, sum * A[i]);
+		for (int i = mul; i < mul_cnt; i++) {
+			dfs(plus, minus, mul + 1, div, sum * A[depth], depth+1);
 		}
-		for (int i = 0; i < div_cnt; i++) {
-			dfs(plus, minus, mul, div + 1, sum / A[i]);
+		for (int i = div; i < div_cnt; i++) {
+			dfs(plus, minus, mul, div + 1, sum / A[depth], depth+1);
 		}
+		
 		
 	}
 
--- Algorithm/src/test/test.java
+++ Algorithm/src/test/test.java
@@ -9,9 +9,63 @@ public class test {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		input = br.readLine();
-		input += "??!";
-		System.out.println(input);
+		
+//		System.out.println(Math.atan2(3, 2));
+//		System.out.println(Math.atan(0.44) * 180 / Math.PI); // 
+		
+		float white_ball_y = 127;
+		float white_ball_x = 63.5f;
+		
+		float ball_y = 240;
+		float ball_x = 110;
+		
+		float w = Math.abs(white_ball_x - ball_x);
+		float h =  Math.abs(white_ball_y - ball_y);
+		float distance = (float)Math.sqrt( Math.pow( h , 2)  + Math.pow( w , 2) );	
+		System.out.println(w/h);
+		
+		System.out.println( Math.atan( (h/w) ) * 180 / Math.PI ); //라디안 각도 -> 일반 각도로 변환 
+		
+//		System.out.println(Math.atan( (8.0/6.0) ) * 180 / Math.PI);
+		System.out.println(Math.atan2(h,w)  * 180 / Math.PI );
+		
+		System.out.println(Math.sqrt((90*90 + 50*50)) + 5.73);
+		System.out.println(5.73/2 + 10);
+		
+		System.out.println(Math.sqrt((105.73*105.73) - (105.205*105.205)));
+		
+		
+		int hole_x = 127;
+		int hole_y = 0;
+		int goal_x = 10;
+		int goal_y = 100;
+		int white_x = 100;
+		int white_y = 50;
+		double r = 5.73; // 당구공 지름 
+		// 홀에서 목표구까지의 거리 
+		double diff_x = Math.abs( goal_x - hole_x ); 
+		double diff_y = Math.abs( goal_y - hole_y );
+		
+		double d = Math.sqrt(  (diff_x * diff_x)   +  (diff_y * diff_y) );
+		double D = d + r;
+		double se = (Math.atan2( goal_y , goal_x ) * 180 / Math.PI); // 세타 
+		double H = Math.cos(se) * D;
+		double W = Math.sin(se) * D; // 또는 피타고라서 정리로도 가능 
+		
+		// 가상 목표 지점 W, H 구하기 완료
+		
+		// 흰 공과 가상 목표 지점과의 각도 구하기
+		diff_x = Math.abs( W - white_x ); 
+		diff_y = Math.abs( H - white_y );
+		d = Math.sqrt(  (diff_x * diff_x)   +  (diff_y * diff_y) );
+		
+		Math.atan2(diff_x, diff_y);
+	
+		
+		System.out.println(  Math.atan2(35, 65 ) * 180  / Math.PI  );
+		
+		
+		
 	}
 
 }
