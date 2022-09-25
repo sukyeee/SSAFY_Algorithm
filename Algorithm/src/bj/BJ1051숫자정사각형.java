9465867145610b6101e3895be487819ae5f28238
이수경 <1724271@donga.ac.kr>
2022-09-25 21:52:16 +0900

    BJ 1051 숫자정사각형

--- Algorithm/src/bj/BJ1051숫자정사각형.java
+++ Algorithm/src/bj/BJ1051숫자정사각형.java
@@ -9,6 +9,7 @@ public class BJ1051숫자정사각형 {
 	static int N, M;
 	static int map[][];
 	static int max;
+	static int result;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -26,12 +27,22 @@ public class BJ1051숫자정사각형 {
 		// 입력 완
 		
 		// 정사각형 찾기
-		int left_top = -1, right_top = -1, left_bottom = -1, right_bottom = -1;
+		int K = Math.min(N, M);
 		
-
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				for (int k = 0; k < K; k++) {
+					
+					if( i + k >= N || j + k >= M ) continue;
+					
+					if( map[i][j] == map[i+k][j] &&  map[i][j] == map[i][j+k] && map[i][j] == map[i+k][j+k] )
+					result = Math.max(result, k+1);
+				}
+			}
+		}
 		
 		
-		
+		System.out.println(result*result);
 		
 	}
 }
