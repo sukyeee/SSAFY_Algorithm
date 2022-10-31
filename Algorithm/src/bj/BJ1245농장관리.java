41c2a95064ec795194f583365a637e04f1f9d39a
sukyeee <1724271@donga.ac.kr>
2022-10-31 13:58:56 +0900

    2022/10/31

---
+++ Algorithm/src/bj/BJ1245농장관리.java
@@ -0,0 +1,18 @@
+package bj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1245농장관리 {
+
+	static int N, M;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+	}
+
+}
